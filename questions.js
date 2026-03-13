262be0c9ad2b19e87605d721984b5c333465edb5
aswinkrish8-design <aswin.krish8@gmail.com>
2026-03-13 15:19:36 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,21 @@
+const questions = [
+
+{
+question:"What is 2 + 2?",
+options:["1","2","3","4"],
+answer:3
+},
+
+{
+question:"Derivative of x^2?",
+options:["x","2x","x^2","1"],
+answer:1
+},
+
+{
+question:"Speed of light?",
+options:["3×10^8","10^8","3×10^6","10^6"],
+answer:0
+}
+
+]
