793fe330aea32f915742e83afe87fab316367b45
aswinkrish8-design <aswin.krish8@gmail.com>
2026-03-13 15:20:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+let current = 0
+let answers = []
+
+loadQuestion()
+
+function loadQuestion(){
+
+let q = questions[current]
+
+document.getElementById("question").innerText = q.question
+
+let html = ""
+
+q.options.forEach((opt,i)=>{
+
+html += `
+<label>
+<input type="radio" name="option" value="${i}">
+${opt}
+</label><br>
+`
+
+})
+
+document.getElementById("options").innerHTML = html
+
+}
+
+function nextQuestion(){
+
+let selected = document.querySelector('input[name="option"]:checked')
+
+if(selected){
+answers[current] = parseInt(selected.value)
+}
+
+current++
+
+if(current < questions.length){
+loadQuestion()
+}
+
+}
+
+function submitExam(){
+
+let name = prompt("Enter your name")
+
+let score = 0
+
+for(let i=0;i<questions.length;i++){
+
+if(answers[i] == questions[i].answer){
+score++
+}
+
+}
+
+fetch("YOUR_GOOGLE_SCRIPT_URL",{
+
+method:"POST",
+
+body:JSON.stringify({
+name:name,
+answers:answers,
+score:score
+})
+
+})
+
+alert("Exam submitted. Score: "+score)
+
+}
